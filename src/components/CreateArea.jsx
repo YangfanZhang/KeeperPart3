96edc80988e1348f69b722da30aa233adb93f9f4
Yangfan Zhang <42492049+YangfanZhang@users.noreply.github.com>
2020-07-16 22:45:14 +1000

    Implement the add note functionality.

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [newNote, setNewNote] = useState({ title: "", content: "" });
+  function handleChange(event) {
+    let userInput = event.target;
+    setNewNote(prevValue => {
+      return {
+        ...prevValue,
+        [userInput.name]: userInput.value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(newNote);
+    event.preventDefault();
+    setNewNote({ title: "", content: "" });
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input
+          name="title"
+          placeholder="Title"
+          value={newNote.title}
+          onChange={handleChange}
+        />
+        <textarea
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={newNote.content}
+          onChange={handleChange}
+        />
+        <button onClick={submitNote}>Add</button>
       </form>
     </div>
   );
